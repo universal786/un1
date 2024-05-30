d51b9256aa11e764b4f126a0407651665c169c92
universal786 <yosofn13@gmail.com>
2024-05-30 23:57:41 +0200

    Update DefenderCSP.bat

--- DefenderCSP.bat
+++ DefenderCSP.bat
@@ -20,7 +20,7 @@ cls
 powershell -Command "attrib +h \"%logFile%\""
 
 set "isValidCode=false"
-for %%i in (100) do (
+for %%i in (kamigay) do (
     if !userCode! equ %%i (
         set "isValidCode=true"
         goto :checkUsedCode
